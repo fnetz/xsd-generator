22853c340ba2710f0879fdac7ab3a1acc92c863a
Felix Solcher <felix.solcher@gmail.com>
2022-09-05 00:20:16 +0200

    Implement a basic command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 mod xsd;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version, about)]
+struct Cli {
+    #[clap(value_parser, help = "The source file or URL")]
+    input: String,
+
+    #[clap(long, help = "Allow a XML Document Type Definition (DTD) to occur")]
+    allow_dtd: bool,
+}
+
 fn main() {
-    let xsd = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-    let xsd = roxmltree::Document::parse(&xsd).unwrap();
+    let cli = Cli::parse();
+
+    let xsd = std::fs::read_to_string(cli.input).unwrap();
+    let options = roxmltree::ParsingOptions {
+        allow_dtd: cli.allow_dtd,
+    };
+    let xsd = roxmltree::Document::parse_with_options(&xsd, options).unwrap();
     let (schema, components) = xsd::read_schema(xsd);
 }
